266f4fc6e629adb1e9dddf249202166da309c2c7
Jay Thanki <thankijay99@gmail.com>
2023-10-03 09:16:47 +0100

    Added set and get method

---
+++ src/ht.h
@@ -0,0 +1,32 @@
+#ifndef _HT_H
+#define _HT_H
+
+
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef struct ht ht;
+
+ht* ht_create(void);
+
+void ht_destroy(ht* table);
+
+void* ht_get(ht* table, const char* key);
+
+const char* ht_set(ht* table, const char* key, void* value);
+
+size_t ht_length(ht* table);
+
+typedef struct {
+    const char* key;  
+    void* value;     
+
+    ht* _table;       
+    size_t _index;   
+} hti;
+
+hti ht_iterator(ht* table);
+
+bool ht_next(hti* it);
+
+#endif
